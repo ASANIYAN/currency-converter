2dbeb97f9861347154b4809cf6065f661ae5f634
Asaniyan Ayomide Paul <ayomidepaulasaniyan@gmail.com>
2025-10-21 12:42:05 +0100

    Switch to Neon PostgreSQL with SSL support

--- src/config/database.ts
+++ src/config/database.ts
@@ -17,6 +17,9 @@ export const connectDatabase = async (): Promise<Pool> => {
     max: 20, // Maximum number of clients in the pool
     idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
     connectionTimeoutMillis: 2000, // Return error after 2 seconds if can't connect
+    ssl: {
+      rejectUnauthorized: false, // Required for Neon
+    },
   });
 
   pool.on("connect", () => {
